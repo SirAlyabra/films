22ab117f261a805460972769afb61e4713cd16b6
SirAlyabra <alyabra.vargas@gmail.com>
2022-07-20 12:40:04 -0500

    Boton like

--- src/main.js
+++ src/main.js
@@ -37,9 +37,9 @@ function createMovies(movies,
         // const categoriesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
-        movieContainer.addEventListener('click', () => {
-            location.hash = 'movie='+ movie.id;
-        })
+        // movieContainer.addEventListener('click', () => {
+        //     location.hash = '#movie='+ movie.id;
+        // });
 
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
@@ -48,16 +48,26 @@ function createMovies(movies,
             lazyLoad ? 'data-img': 'src',
             'https://image.tmdb.org/t/p/w300/'+ movie.poster_path
         );
+        movieImg.addEventListener('click', () => {
+            location.hash = '#movie='+ movie.id;
+        });
         movieImg.addEventListener('error', () => {
             movieImg.setAttribute('src',"https://image.tmdb.org/t/p/w300/adOzdWS35KAo21r9R4BuFCkLer6.jpg");
         });
 
+        const movieBtn = document.createElement('button');
+        movieBtn.classList.add('movie-btn');
+        movieBtn.addEventListener('click', () => {
+            movieBtn.classList.toggle('movie-btn--liked')
+            //agregar pelicula local storange
+        } )
 
         if(lazyLoad) {
             lazyLoader.observe(movieImg);
         }
 
          movieContainer.appendChild(movieImg);
+         movieContainer.appendChild(movieBtn);
          container.appendChild(movieContainer);
     });
 }
